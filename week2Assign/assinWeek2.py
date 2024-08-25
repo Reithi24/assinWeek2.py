1067a394003ba0724ffaa725b66fb106af2310bb
CLEOPHUS MAINA <mainacleophus@gmail.com>
2024-08-25 15:53:03 +0300

    Add files via upload

---
+++ week2Assign/assinWeek2.py
@@ -0,0 +1,26 @@
+
+#Insert the value 15 at the second position in the list.
+# my_list=[10,20,30,40]
+# my_list.insert(1,15)
+# print(my_list)
+
+#Extend my_list with another list: [50, 60, 70].
+# my_list=[10,20,30,40]
+# numbers=[50,60,70]
+# my_list.extend(numbers)
+# print(my_list)
+
+#Remove the last element from my_list
+# my_list=[10,20,30,40,60,70]
+# del my_list[5]
+# print(my_list)
+
+# #Sort my_list in asscending order.
+# my_list=[10,20,30,40,60,70]
+# my_list.sort()
+# print(my_list)
+
+#Find and print the index of the value 30 in my_list
+# my_list=[10,20,30,40,60,70]
+# index=[2]
+# print(index)
